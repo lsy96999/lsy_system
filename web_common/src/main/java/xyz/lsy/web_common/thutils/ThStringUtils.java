15f9f5a9308f8c173e753194c7f48329afba38d9
noo624 <lsy96999@gmail.com>
2022-11-27 06:40:17 +0900

    web_common 추가

---
+++ web_common/src/main/java/xyz/lsy/web_common/thutils/ThStringUtils.java
@@ -0,0 +1,9 @@
+package xyz.lsy.web_common.thutils;
+
+import org.springframework.stereotype.Component;
+import xyz.lsy.utils.StringUtils;
+
+@Component
+public class ThStringUtils extends StringUtils{
+
+}
