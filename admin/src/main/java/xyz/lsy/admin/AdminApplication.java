b53ceb9793c4e323b8ccbccc65283763f3f79cf0
noo624 <lsy96999@gmail.com>
2022-11-27 16:07:49 +0900

    yml profile별 관리 설정

--- admin/src/main/java/xyz/lsy/admin/AdminApplication.java
+++ admin/src/main/java/xyz/lsy/admin/AdminApplication.java
@@ -7,12 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("xyz.lsy")
 public class AdminApplication {
-	static {
-//		System.setProperty("spring.config.name","application,application-admin");
-	}
 	public static void main(String[] args) {
-//		System.setProperty("spring.config.name","application,application-admin");
-//		System.setProperty("spring.config.name","application,application-admin,application-web_common");
 		SpringApplication.run(AdminApplication.class, args);
 	}
 }
--- admin/src/main/java/xyz/lsy/admin/mvc/controller/SampleRestController.java
+++ admin/src/main/java/xyz/lsy/admin/mvc/controller/SampleRestController.java
@@ -7,9 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleRestController {
 	@Value("${test.common:as}") private String common;
+	@Value("${real.common:as}") private String real;
+	@Value("${real.prof:as}") private String prof;
 	
 	@RequestMapping("/admin/test")
 	public String asdf() {
 		return common;
+	}
+	
+	@RequestMapping("/admin/real")
+	public String real() {
+		return real;
+	}
+	
+	@RequestMapping("/admin/prof")
+	public String prof() {
+		return prof;
 	}
 }
